4fcc982389644fc863b44e9fd743da14fb327787
liyanlei <liyanlei@163.com>
2019-06-27 10:48:35 +0800

    提交

---
+++ day02_GitHup/app/src/main/java/com/example/a41845/day02_githup/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.a41845.day02_githup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
